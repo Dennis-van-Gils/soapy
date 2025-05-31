630590b0e9bba3fd0236c7f0c89d5d2ab9d2fe49
Dennis-van-Gils <vangils.dennis@gmail.com>
2025-05-31 21:17:59 +0200

    Update runProcessChain-dvg.sh

--- simulationCases/runProcessChain-dvg.sh
+++ simulationCases/runProcessChain-dvg.sh
@@ -2,6 +2,7 @@
 # WARNING: Will delete the log, dump and intermediate files
 
 EXE_NAME=soapBubble-full-dvg
+ORIG_DIR=${PWD}
 
 COLOR='\033[0;33m' # Yellow
 NC='\033[0m'       # No Color
@@ -12,12 +13,12 @@ echo -e "${COLOR}Deleting file: dump${NC}"
 rm dump
 echo -e "${COLOR}Deleting file: log${NC}"
 rm log
-
 echo -e "${COLOR}Deleting intermediate files:${NC}"
-cd intermediate/
-ls snapshot*.* -l -h
-rm snapshot*.*
-cd ..
+ls intermediate/snapshot*.* -l -h
+rm intermediate/snapshot*.*
+echo -e "${COLOR}Deleting post process video files:${NC}"
+ls ../postProcess/Video/*.png -l -h
+rm ../postProcess/Video/*.png
 
 # COMPILE
 # ---------------
@@ -29,7 +30,14 @@ qcc -O2 -Wall -fopenmp -disable-dimensions $EXE_NAME.c -o $EXE_NAME -lm
 echo -e "\n${COLOR}Running ${EXE_NAME}${NC}"
 ./$EXE_NAME
 
+# Video
+# ---------------
+echo -e "\n${COLOR}Post processing ${EXE_NAME}${NC}"
+cd ../postProcess/
+python3 ./Video-generic.py
+cd $ORIG_DIR
 
 echo -e "\033[0;32m"
 #read -p "Press any key to finish...""
+echo "Finished."
 echo -e "${NC}"
